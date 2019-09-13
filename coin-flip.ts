955a1e84ab42170aa2a453ade1dee5ad67a9582a
Gabriel Cardona <gabriel@bitcoin.com>
2019-09-13 10:38:52 -0700

    verify and validate user input. log final outcome.

--- coin-flip.ts
+++ coin-flip.ts
@@ -18,13 +18,33 @@ let main: Function = async (): Promise<void> => {
       async (err: any, result: any): Promise<any> => {
         const first: string = result.first
         const second: string = result.second
+
+        // verify user input
+        if (!first) {
+          throw "Must provide first address"
+        }
+
+        if (!second) {
+          throw "Must provide second address"
+        }
+
+        // validate user input
+        if (!SLP.Address.isSLPAddress(first)) {
+          throw "First address isn't SLP format"
+        }
+        if (!SLP.Address.isSLPAddress(second)) {
+          throw "Second address isn't SLP format"
+        }
+        let winner: string = first
         let addy: string = first
         if (outcome === "heads") {
           addy = first
+          winner = first
         } else {
           addy = second
+          winner = second
         }
-        let send = await SLP.TokenType1.send({
+        let txid: string = await SLP.TokenType1.send({
           fundingAddress: "slptest:qp374ny8hjkwede9msydwxtux2lhj3fesyukyvlsfg",
           fundingWif: "cPXh1nTCfCaP31GdGArpJa3uNwEDG9i7Jwb2hSzehUczJcWfieJP",
           tokenReceiverAddress: addy,
@@ -34,8 +54,15 @@ let main: Function = async (): Promise<void> => {
             "eb08f3ceb2936285e438e712a17cd0a1dcf5bf818eeeb563f4b3360e5d201e7e",
           amount: 1
         })
-        console.log(outcome)
-        console.log(send)
+
+        // log outcome and winner
+        console.log(`The outcome is: ${outcome}!`)
+        console.log(
+          `The winner is: https://explorer.bitcoin.com/tbch/address/${SLP.Address.toSLPAddress(
+            winner
+          )}!`
+        )
+        console.log(`Txid: https://explorer.bitcoin.com/tbch/tx/${txid}#o=1`)
       }
     )
   } catch (err) {
